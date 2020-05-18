5195819bb39b902acb2afad82732d3116d514587
ftToti <ft.1rjn@gmail.com>
2020-05-18 22:38:46 +0900

    自分での動作確認完了

--- bookers2-debug/app/controllers/books_controller.rb
+++ bookers2-debug/app/controllers/books_controller.rb
@@ -1,15 +1,22 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
 
   def show
   	@book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @book.user_id = current_user.id
+
+    # @book = current_user.books.new(book_params)
+    # ==============
   	if @book.save #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
@@ -33,16 +40,16 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.delete
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
 
 end
