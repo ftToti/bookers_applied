cdba121bf6e1f4853000f56dd5ba5945d7659c21
ftToti <ft.1rjn@gmail.com>
2020-05-21 06:21:46 +0900

    [Start]bookers5検索機能

---
+++ bookers5/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'home#top'
+  get 'home/about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resource :book_comments, only: [:create, :destroy]
+  end
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+  get 'user/:id/follow' => 'users#follow', as: 'user_follow'
+end
